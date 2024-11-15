882faf2b9e29a385a66dc7974f6be8126d1a853c
Mnezello <m.nezello.18@hotmail.com>
2024-11-15 18:39:01 -0300

    correct_setting

--- src/services/sanity-client.js
+++ src/services/sanity-client.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export default sanityClient({
-  projectId: "0xv60lnd", 
+  projectId: "******", 
   dataset: "production",
   apiVersion: 'v2021-06-07',
   useCdn: true
